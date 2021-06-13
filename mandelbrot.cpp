dd8c173649a94abacc9746fb5ce249254a4fc62d
Nelson <nelsonsmmartins@gmail.com>
2021-06-12 23:56:47 -0300

    Divisão das threads e implementação do buffer.

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -2,162 +2,300 @@
 #include <mpi.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int WINDOWSIZE = 200;
+int NUM_THREADS_REQUIRED = 2;
+int WINDOWSIZE = 1000;
+int MAX_ITERATIONS = 100;
 
 long double min = -2.0;
 long double max = 2.0;
-int MAX_ITERATIONS = 100;
 
-long double map(long double value, long double in_min, long double in_max, long double out_min, long double out_max)
-{
-	return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
+char EXT[5] = ".buf";
+
+bool DEBUG_CONSOLE = false;
 
 typedef struct workerBuffer
 {
-	int initialX;
-	int initialY;
-	int finalX;
-	int finalY;
+    int initialX;
+    int initialY;
+    int finalX;
+    int finalY;
+    int rank;
 } worker;
 
-typedef struct resultBuffer
+long double map(long double value, long double in_min, long double in_max, long double out_min, long double out_max)
 {
-	int X;
-	int Y;
-	int bright;
-} result;
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
+int maxDiv(int value)
+{
+    int valueC = value - 1;
+    while (value % valueC != 0 && valueC > 1)
+    {
+        valueC--;
+    }
+    if (valueC == 1)
+    {
+        return value;
+    }
+    return valueC;
+}
+
+void getName(char* name, int rank)
+{
+    sprintf(name, "%d%s", rank, EXT);
+}
+
+void divThread(int sizeReal, MPI_Datatype sendType)
+{
+    float columnCount = sqrt(sizeReal);
+    int rowCount = (int) columnCount;
+
+    //NAO E RAIZ EXATA
+    if (columnCount != rowCount)
+    {
+        columnCount = maxDiv(sizeReal);
+        rowCount = sizeReal / (int) columnCount;
+    }
+
+    int lengthX = (int) WINDOWSIZE / columnCount;
+    int lengthDiffX = WINDOWSIZE - (lengthX * columnCount);
+    int lengthY = (int) WINDOWSIZE / rowCount;
+    int lengthDiffY = WINDOWSIZE - (lengthY * rowCount);
+    int initialX = 0;
+    int initialY = 0;
+    int finalX = lengthX;
+    int finalY = lengthY;
+
+    bool useLengthDiffX = lengthDiffX > 0;
+    bool useLengthDiffY = lengthDiffY > 0;
+
+    if (DEBUG_CONSOLE)
+    {
+        printf ("LENGTH X %d\n", lengthX);
+        printf ("LENGTH DIF X %d\n", lengthDiffX);
+        printf ("COLUMN COUNT %f.2\n", columnCount);
+        printf ("ROW COUNT %d\n", rowCount);
+    }
+
+    int j = 1;
+    for (int i = NUM_THREADS_REQUIRED; i < sizeReal + NUM_THREADS_REQUIRED; i++)
+    {
+        bool resetInitialX = (i - 1) % ((int) columnCount) == 0;
+
+        if (useLengthDiffX && resetInitialX)
+        {
+            finalX += lengthDiffX;
+        }
+
+        worker send;
+        send.initialX = initialX;
+        send.initialY = initialY;
+        send.finalX = finalX;
+        send.finalY = finalY;
+        send.rank = i;
+
+        MPI_Send(&send, 1, sendType, i, 0, MPI_COMM_WORLD);
+
+        //FAZ O PROCESSAMENTO DAS NOVAS POSICOES PARA A PROXIMA THREAD
+        if (resetInitialX)
+        {
+            j++;
+        }
+
+        bool lastTY = j == rowCount;
+
+        if (DEBUG_CONSOLE)
+        {
+            printf ("RANK %d\n", i);
+            printf ("INITIAL X %d\n", initialX);
+            printf ("INITIAL Y %d\n", initialY);
+            printf ("FINAL X %d\n", finalX);
+            printf ("FINAL Y %d\n", finalY);
+        }
+
+        //ATRIBUI OS VALORES INICIAIS CORRETOS PARA A PROXIMA THREAD
+        if (resetInitialX)
+        {
+            initialX = 0;
+            initialY = finalY;
+        }
+        else
+        {
+            initialX = finalX;
+        }
+
+        //CALCULO DO TAMANHO NO EIXO X PARA A PROXIMA THREAD
+        finalX += lengthX;
+        if (resetInitialX)
+        {
+            finalY += lengthY;
+            finalX = lengthX;
+        }
+
+        //CALCULO DO TAMANHO NO EIXO Y PARA A PROXIMA THREAD
+        if (lastTY && useLengthDiffY)
+        {
+            finalY = lengthY * rowCount + lengthDiffY;
+        }
+    }
+}
+
+void printWindow(int sizeReal)
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+    SDL_CreateWindowAndRenderer(WINDOWSIZE, WINDOWSIZE, 0, &window, &renderer);
+    SDL_RenderSetLogicalSize(renderer, WINDOWSIZE, WINDOWSIZE);
+    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+    SDL_RenderClear(renderer);
+    MPI_Status status;
+    for (int i = 0; i < sizeReal; i++)
+    {
+        SDL_RenderPresent(renderer);
+        int recv;
+        char name[100];
+        MPI_Recv(&recv, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+        getName(name, recv);
+        FILE *fBuffer = fopen(name, "r");
+        size_t len = 100;
+        char *line = (char *) malloc(len);
+        while (getline(&line, &len, fBuffer) > 0)
+        {
+            int x = atoi(line);
+            getline(&line, &len, fBuffer);
+            int y = atoi(line);
+            getline(&line, &len, fBuffer);
+            int brigth = atoi(line);
+            SDL_SetRenderDrawColor(renderer, brigth, brigth, brigth, 255);
+            SDL_RenderDrawPoint(renderer, x, y);
+        }
+        remove(name);
+    }
+    SDL_RenderPresent(renderer);
+    usleep(100000000);
+}
+
+void slave(MPI_Datatype recType)
+{
+    MPI_Status status;
+    worker recv;
+    MPI_Recv(&recv, 1, recType, 0, 0, MPI_COMM_WORLD, &status);
+    char name[100];
+    getName(name, recv.rank);
+    FILE *fBuffer = fopen(name, "w+");
+    if (DEBUG_CONSOLE)
+    {
+        printf ("RANK - CALCULO %d\n", recv.rank);
+        printf ("INITIAL X %d\n", recv.initialX);
+        printf ("INITIAL Y %d\n", recv.initialY);
+        printf ("FINAL X %d\n", recv.finalX);
+        printf ("FINAL Y %d\n", recv.finalY);
+    }
+
+    for (int x = recv.initialX; x < recv.finalX; x++)
+    {
+        for (int y = recv.initialY; y < recv.finalY; y++)
+        {
+
+            //Mapping the size of the window to (-2.0,2.0)
+
+            long double a = map(x, 0, WINDOWSIZE, min, max); //a = x
+            long double b = map(y, 0, WINDOWSIZE, min, max); //b = y
+
+            //Mandelbrot set
+
+            long double aInitial = a;
+            long double bInitial = b;
+
+            int count = 0;
+
+            for (int i = 0; i < MAX_ITERATIONS; i++)
+            {
+                //x^2 + 2xyi - y^2
+                long double a1 = a * a - b * b;
+                long double b1 = 2 * a * b;
+
+                a = a1 + aInitial;
+                b = b1 + bInitial;
+
+                if ((a + b) > 2)
+                {
+                    break;
+                }
+
+                count++;
+            }
+
+            int bright = map(count, 0, MAX_ITERATIONS, 0, 255);
+
+            if ((count == MAX_ITERATIONS) || (bright < 20))
+            {
+                bright = 0;
+            }
+            fprintf(fBuffer, "%d\n", x);
+            fprintf(fBuffer, "%d\n", y);
+            fprintf(fBuffer, "%d\n", bright);
+        }
+    }
+    fclose(fBuffer);
+    MPI_Send(&recv.rank, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+}
 int main(int argc, char *argv[])
 {
-	int rank, size, i;
-	MPI_Status status;
+    int rank, size, sizeReal, i;
+    MPI_Status status;
 
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-	/* create a type for struct workerBuffer */
-	const int nitems = 4;
-	int blocklengths[4] = {1, 1, 1, 1};
-	MPI_Datatype types[4] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT};
-	MPI_Datatype mpi_worker_type;
-	MPI_Aint offsets[4];
+    sizeReal = size - NUM_THREADS_REQUIRED;
 
-	offsets[0] = offsetof(worker, initialX);
-	offsets[1] = offsetof(worker, initialY);
-	offsets[2] = offsetof(worker, finalX);
-	offsets[3] = offsetof(worker, finalY);
+    if (sizeReal <= 1)
+    {
+        printf("Numero minimo (%d) de threads nao informado.\n", NUM_THREADS_REQUIRED);
+        MPI_Finalize();
+        return 1;
+    }
 
-	MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_worker_type);
-	MPI_Type_commit(&mpi_worker_type);
+    /* create a type for struct workerBuffer */
+    const int nitems = 5;
+    int blocklengths[5] = {1, 1, 1, 1, 1};
+    MPI_Datatype types[5] = {MPI_INT, MPI_INT, MPI_INT, MPI_INT, MPI_INT};
+    MPI_Datatype mpi_worker_type;
+    MPI_Aint offsets[5];
 
-	const int nitems2 = 3;
-	int blocklengths2[3] = {1, 1, 1};
-	MPI_Datatype types2[3] = {MPI_INT, MPI_INT, MPI_INT};
-	MPI_Datatype mpi_result_type;
-	MPI_Aint offsets2[3];
+    offsets[0] = offsetof(worker, initialX);
+    offsets[1] = offsetof(worker, initialY);
+    offsets[2] = offsetof(worker, finalX);
+    offsets[3] = offsetof(worker, finalY);
+    offsets[4] = offsetof(worker, rank);
 
-	offsets2[0] = offsetof(result, X);
-	offsets2[1] = offsetof(result, Y);
-	offsets2[2] = offsetof(result, bright);
+    MPI_Type_create_struct(nitems, blocklengths, offsets, types, &mpi_worker_type);
+    MPI_Type_commit(&mpi_worker_type);
 
-	MPI_Type_create_struct(nitems2, blocklengths2, offsets2, types2, &mpi_result_type);
-	MPI_Type_commit(&mpi_result_type);
+    switch(rank)
+    {
+    case 0:
+        divThread(sizeReal, mpi_worker_type);
+        break;
+    case 1:
+        printWindow(sizeReal);
+        break;
+    default:
+        slave(mpi_worker_type);
+        break;
+    }
 
-	if (rank == 0)
-	{
-		worker send;
-		send.initialX = 0;
-		send.initialY = 0;
-		send.finalX = WINDOWSIZE;
-		send.finalY = WINDOWSIZE;
-		MPI_Send(&send, 1, mpi_worker_type, 2, 0, MPI_COMM_WORLD);
-	}
-	else if (rank == 1)
-	{
-
-		SDL_Init(SDL_INIT_EVERYTHING);
-		SDL_Window *window;
-		SDL_Renderer *renderer;
-		SDL_Event event;
-		SDL_CreateWindowAndRenderer(WINDOWSIZE, WINDOWSIZE, 0, &window, &renderer);
-		SDL_RenderSetLogicalSize(renderer, WINDOWSIZE, WINDOWSIZE);
-		SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
-		SDL_RenderClear(renderer);
-
-		for (int i = 0; i < WINDOWSIZE * WINDOWSIZE; i++)
-		{
-			SDL_RenderPresent(renderer);
-			result recv;
-			MPI_Recv(&recv, 1, mpi_result_type, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-			//printf("X = %i, Y = %i, bright = %i", recv.X, recv.Y, recv.bright);
-
-			SDL_SetRenderDrawColor(renderer, recv.bright, recv.bright, recv.bright, 255);
-			SDL_RenderDrawPoint(renderer, recv.X, recv.Y);
-		}
-		usleep(10000000);
-	}
-	else if (rank == 2)
-	{
-		worker recv;
-		MPI_Recv(&recv, 1, mpi_worker_type, 0, 0, MPI_COMM_WORLD, &status);
-
-		result send;
-
-		for (int x = recv.initialX; x < recv.finalX; x++)
-		{
-			for (int y = recv.initialY; y < recv.finalY; y++)
-			{
-
-				//Mapping the size of the window to (-2.0,2.0)
-
-				long double a = map(x, 0, WINDOWSIZE, min, max); //a = x
-				long double b = map(y, 0, WINDOWSIZE, min, max); //b = y
-
-				//Mandelbrot set
-
-				long double aInitial = a;
-				long double bInitial = b;
-
-				int count = 0;
-
-				for (int i = 0; i < MAX_ITERATIONS; i++)
-				{
-					//x^2 + 2xyi - y^2
-					long double a1 = a * a - b * b;
-					long double b1 = 2 * a * b;
-
-					a = a1 + aInitial;
-					b = b1 + bInitial;
-
-					if ((a + b) > 2)
-					{
-						break;
-					}
-
-					count++;
-				}
-
-				int bright = map(count, 0, MAX_ITERATIONS, 0, 255);
-
-				if ((count == MAX_ITERATIONS) || (bright < 20))
-				{
-					bright = 0;
-				}
-
-				send.X = x;
-				send.Y = y;
-				send.bright = bright;
-				MPI_Send(&send, 1, mpi_result_type, 1, 0, MPI_COMM_WORLD);
-			}
-		}
-		printf("X = %i, Y = %i, bright = %i", send.X, send.Y, send.bright);
-	}
-
-	MPI_Type_free(&mpi_worker_type);
-	MPI_Type_free(&mpi_result_type);
-	MPI_Finalize();
-	return 0;
+    MPI_Type_free(&mpi_worker_type);
+    MPI_Finalize();
+    return 0;
 }
